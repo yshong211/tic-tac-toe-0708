8d44678edde68bff0c01ebe89d057c90beaded56
System Administrator <root@vcv076159.vpn.uci.edu>
2020-07-08 05:28:13 +0900

    Updates

---
+++ precache-manifest.3afcb4ec47619955e6b38758c5a701fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adba8d796d47aa1a321a311871aee505",
+    "url": "/tic-tac-toe-0708/index.html"
+  },
+  {
+    "revision": "62e1d1c27bf49aefb5e5",
+    "url": "/tic-tac-toe-0708/static/css/main.97769e27.chunk.css"
+  },
+  {
+    "revision": "199b519ec2925e2af82b",
+    "url": "/tic-tac-toe-0708/static/js/2.d46db5b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe-0708/static/js/2.d46db5b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62e1d1c27bf49aefb5e5",
+    "url": "/tic-tac-toe-0708/static/js/main.a3a1c064.chunk.js"
+  },
+  {
+    "revision": "b4e7d8f0e8429e460581",
+    "url": "/tic-tac-toe-0708/static/js/runtime-main.62848337.js"
+  }
+]);
