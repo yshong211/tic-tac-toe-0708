4f27baf60f046062644e3bc92bff2205615bb21d
System Administrator <root@Bradyui-MacBook-Pro.local>
2020-07-09 12:29:11 +0900

    Updates

--- precache-manifest.5749442920a0ae963fc926343d6e04d2.js
+++ precache-manifest.5749442920a0ae963fc926343d6e04d2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "adba8d796d47aa1a321a311871aee505",
+    "revision": "fa1f7e437888847cbfc65dd7fe7b24ad",
     "url": "/tic-tac-toe-0708/index.html"
   },
   {
-    "revision": "62e1d1c27bf49aefb5e5",
-    "url": "/tic-tac-toe-0708/static/css/main.97769e27.chunk.css"
+    "revision": "8d9889048230a5524bea",
+    "url": "/tic-tac-toe-0708/static/css/main.89b996ee.chunk.css"
   },
   {
     "revision": "199b519ec2925e2af82b",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/tic-tac-toe-0708/static/js/2.d46db5b5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "62e1d1c27bf49aefb5e5",
-    "url": "/tic-tac-toe-0708/static/js/main.a3a1c064.chunk.js"
+    "revision": "8d9889048230a5524bea",
+    "url": "/tic-tac-toe-0708/static/js/main.4ccc731a.chunk.js"
   },
   {
     "revision": "b4e7d8f0e8429e460581",
